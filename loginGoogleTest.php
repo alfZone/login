6977631cc46e0c1e489df035e1cf1af180ffb8b6
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-04-13 16:47:13 +0100

    Update loginGoogleTest.php

--- loginGoogleTest.php
+++ loginGoogleTest.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
-  <meta charset="utf-8"/>
-  <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
-  <meta name="description" content="Login da Google"/>
-  <meta name="author" content="António Lira Fernandes"/>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Login da Google">
+  <meta name="author" content="António Lira Fernandes">
   <meta name="google-signin-client_id" content="436091069232-g9n82uk2iua4hpa3unrfrt3iq0gkntb7.apps.googleusercontent.com">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
-  <link rel="icon" type="images/png" sizes="16x16" href="img/looglemin.png"/>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="icon" type="images/png" sizes="16x16" href="img/looglemin.png">
   <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script-->
   <!--script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script-->
-  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  
   <title>Login</title>
 </head>
 
@@ -48,6 +49,19 @@
     </div>
 
   </div>
+  
+  <!-- Bootstrap core JavaScript-->
+  <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
+  <script src="/templates/startbootstrap_admin_2/vendor/jquery/jquery.min.js"></script>
+  <script src="/templates/startbootstrap_admin_2/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="/templates/startbootstrap_admin_2/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <!-- Core plugin JavaScript-->
+  <script src="/templates/startbootstrap_admin_2/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="/templates/startbootstrap_admin_2/js/sb-admin-2.min.js"></script>
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  
   <script>
     function onSignIn(googleUser) {
       var profile = googleUser.getBasicProfile();
@@ -66,7 +80,7 @@
           userPicture: userPicture,
           userEmail: userEmail
         }
-        $.post('public/validacaoLogin', dados, function(retorna) {
+        $.post('http://votacao.alunos.esmonserrate.org/public/login/valida', dados, function(retorna) {
           //alert(retorna)
           //retorna=1;
           //console.log("dsdsd")
@@ -107,16 +121,7 @@
     
     
   </script>
-  <!-- Bootstrap core JavaScript-->
-  <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
-  <script src="/templates/startbootstrap_admin_2/vendor/jquery/jquery.min.js"></script>
-  <script src="/templates/startbootstrap_admin_2/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="/templates/startbootstrap_admin_2/vendor/jquery-easing/jquery.easing.min.js"></script>
-  <!-- Core plugin JavaScript-->
-  <script src="/templates/startbootstrap_admin_2/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-  <!-- Custom scripts for all pages-->
-  <script src="/templates/startbootstrap_admin_2/js/sb-admin-2.min.js"></script>
+  
 
 </body>
 
