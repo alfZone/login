6f0b9a74ad9ac4d3390eaa377eb657e336873077
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-04-14 11:58:50 +0100

    Update loginGoogleTest.php

--- loginGoogleTest.php
+++ loginGoogleTest.php
@@ -1,152 +1,83 @@
 <!DOCTYPE html>
 <html lang="pt">
-  
+
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/png" sizes="16x16" href="/imagens/66276m.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Login da Google">
+  <meta name="author" content="António Lira Fernandes">
   <meta name="google-signin-client_id" content="436091069232-g9n82uk2iua4hpa3unrfrt3iq0gkntb7.apps.googleusercontent.com">
-  <title>Professor| 2do</title>
-
-  <!-- Google Font: Source Sans Pro -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-  <!-- Font Awesome -->
-  <link rel="stylesheet" href="/templates/AdminLTE/plugins/fontawesome-free/css/all.min.css">
-  <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="/templates/AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="/templates/AdminLTE/dist/css/adminlte.min.css">
-
- 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="icon" type="images/png" sizes="16x16" href="/images/looglemin.png">
+  <!--script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script-->
+  <!--script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script-->
+  
+  <title>Login</title>
 </head>
 
-<body class="hold-transition login-page bg-dark ">
-  <div class="login-box">
-    <!-- /.login-logo -->
-    <div class="card card-outline card-primary">
-      <div class="card-header text-center">
-        <a href="" class="h1 text-dark"><b>Administração ESM</a>
-      </div>
-      <div class="card-body">
-        <p class="login-box-msg">Autenticação para entrar no sistema</p>
+<body class="bg-gradient-primary">
 
-        <form action="" method="post">
-          <div class="input-group mb-3">
-            <input type="email" class="form-control" placeholder="Email">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-envelope"></span>
-              </div>
-            </div>
-          </div>
-          <div class="input-group mb-3">
-            <input type="password" class="form-control" placeholder="Password">
-            <div class="input-group-append">
-              <div class="input-group-text">
-                <span class="fas fa-lock"></span>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-6">
-              <div class="icheck-primary">
-                <input type="checkbox" id="remember">
-                <label for="remember" class="text-secondary">
-                Lembrar
-              </label>
-              </div>
-            </div>
-            <!-- /.col -->
-            <div class="col-6">
-              <button type="submit" class="btn btn-primary btn-block">Ligar</button>
-            </div>
-          </div>
-          <div class="row">
-              <div class="col-6">
-                <p for="remember" class="text-secondary">
-                usar o email institucional
-              </p>
-              </div>
-              <div class="col-6">
-                <div class="g-signin2 btn btn-light btn-block" data-onsuccess="onSignIn" data-width="140" ></div>
-              </div>
-          </div>
-            
-            <!-- /.col -->
-          
-        </form>
 
-        <
-        
-        <!-- /.social-auth-links -->
-        <div id="msg" class="text-secondary">     
+  <div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+      <div class="col-xl-10 col-lg-12 col-md-9">
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-5 m-2">
+                <img src="/images/loogle.png"/>
+              </div>
+              <div class="col-lg-6 m-2 text-center">
+                <h1>System Login</h1>
+                <!-- <form class="user">  -->
+                <hr>
+                <div id="my-signin2"></div>
+                <div id="msg"></div>
+             </div>
+              
+           </div>
+          </div>
         </div>
-        <p class="mb-1">
-          <a href="">Não sei a password</a>
-        </p>
-        <p class="mb-0">
-          <a href="" class="text-center">Novo Registo</a>
-        </p>
+
       </div>
-      <!-- /.card-body -->
+
     </div>
-    <!-- /.card -->
+
   </div>
-  <!-- /.login-box -->
-
-  <!-- jQuery -->
-  <script src="/templates/AdminLTE/plugins/jquery/jquery.min.js"></script>
-  <!-- Bootstrap 4 -->
-  <script src="/templates/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <!-- AdminLTE App -->
-  <script src="/templates/AdminLTE/dist/js/adminlte.min.js"></script>
   
-  <script src="https://apis.google.com/js/platform.js" async defer></script>
-  <script src="/js/code/valida.js"></script>
+  <!-- Bootstrap core JavaScript-->
+  <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
+  <!--script src="/templates/startbootstrap_admin_2/vendor/bootstrap/js/bootstrap.bundle.min.js"></script-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
+  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  
   <script>
-    
-    
     function onSignIn(googleUser) {
-      
-      const queryString = window.location.search;
-      const urlParams = new URLSearchParams(queryString);
-      const act = urlParams.get('d')
-      if (act=="k"){
-        //alert(act)
-       var auth2 = gapi.auth2.getAuthInstance();
-        auth2.signOut().then(function () {
-          console.log('User signed out.');
-        });
-        gapi.auth2.getAuthInstance().disconnect();
-        window.location.href = "/public/";
-      }else{
-         //alert("asdasds")
-      //console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
       var profile = googleUser.getBasicProfile();
-      //console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-      //console.log('Name: ' + profile.getName());
-      //console.log('Image URL: ' + profile.getImageUrl());
-      //console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-      var profile = googleUser.getBasicProfile();
-      var userID = profile.getId(); // Do not send to your backend! Use an ID token instead.
+      var userID = profile.getId();
       var userName = profile.getName();
-      var userImageURL = profile.getImageUrl();
+      var userPicture = profile.getImageUrl();
       var userEmail = profile.getEmail();
       var userToken = googleUser.getAuthResponse().id_token;
-      //document.getElementById("msgImg").innerHTML = "<img src='" + userImageURL + "' width='80' height='80' class='rounded'>";
-      document.getElementById("msg").innerHTML = userName;
-      //document.getElementById("btlogin").innerHTML = "<img src='" + userImageURL + "' width='20' height='20' class='rounded'>"
-
+      //alert("dffsdfsdfsdfs");
+      document.getElementById("msg").innerHTML = userEmail;
+      //alert(userEmail)
       if (userEmail !== '') {
         var dados = {
           userID: userID,
           userName: userName,
-          userImageURL: userImageURL,
+          userPicture: userPicture,
           userEmail: userEmail
-        };
-        $.post('/public/validacaoLogin', dados, function(retorna) {
-          //alert(retorna);
+        }
+       alert(dados.userID);
+       $.post('/public/validacaoLogin', dados, function(retorna) {
+          alert(retorna);
           if (retorna === "erro") {
             var msg = "O/A " + userName + " não tem acesso ao sistema!";
             document.getElementById('msg').innerHTML = msg;
@@ -155,22 +86,32 @@
             window.location.href = "/public/admin";
           }
         });
+
       } else {
-        var msg = "Não está nenhuma utilizador autenticado";
-        document.getElementById('msg').innerHTML = msg;
-        document.getElementById('msg').setAttribute("class", "text-danger")
+        var msg = "Utilizador não encontrado";
+        document.getElementById("msg").innerHTML = msg;
       }
-        
-      }
-      
-      
-      
-     
+
 
     }
+
+    function renderButton() {
+      gapi.signin2.render('my-signin2', {
+        'scope': 'profile email',
+        'width': '250px',
+        'height': 37, 
+        'longtitle': true,
+        'theme': 'dark',
+        'onsuccess': onSignIn
+
+      });
+    }
+    
+    
+    
   </script>
   
-  
+
 </body>
 
 </html>
