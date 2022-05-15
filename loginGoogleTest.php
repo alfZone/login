8a2ae4f924518b1ea7fe4f199064b9da481255f9
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-05-15 11:21:38 +0100

    Update loginGoogleTest.php

--- loginGoogleTest.php
+++ loginGoogleTest.php
@@ -52,8 +52,7 @@
   <script src="https://apis.google.com/js/platform.js?onload=renderButton" async defer></script>
   <!--script src="/templates/startbootstrap_admin_2/vendor/bootstrap/js/bootstrap.bundle.min.js"></script-->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-
-  <script src="https://apis.google.com/js/platform.js" async defer></script>
+  <script src="https://accounts.google.com/gsi/client" async defer></script>
   
   <script>
     function onSignIn(googleUser) {
