1f909a2465683a285e68bcb88730cf510b671fe8
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-07-17 15:56:21 +0100

    Create config.js

---
+++ js/code/config.js
@@ -0,0 +1,15 @@
+/**
+ * @author alf
+ * @copyright 2022
+ * @ver 1.0
+ */
+
+
+class config {
+  constructor() {
+  }
+
+  get url() {
+    return  window.location.protocol + "//" + window.location.host + "/";
+  }
+}
