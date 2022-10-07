3dbab7bb9e0f2eefe10367adb932f4b2968e37be
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2022-10-07 16:15:37 +0100

    Update add - routes.php

--- add - routes.php
+++ add - routes.php
@@ -1,5 +1,5 @@
-//Autenticação
-Route::get(['set' => '/autenticacao/getAutentication', 'as' => 'users.getAutentication'], 'ControllerUser@getAutentication');
-Route::get(['set' => '/autenticacao/logout', 'as' => 'users.logout'], 'ControllerUser@logout');
-Route::get('/validacaoLogin', function(){  require _CAMINHO_ADMIN. "valida.php";});
-Route::post('/validacaoLogin', function(){  require _CAMINHO_ADMIN. "valida.php";});
+//Authentication
+Route::get(['set' => '/autenticacao/logout', 'as' => 'loginGoogle.logout'], 'ControllerLoginGoogle@logout');
+Route::get(['set' => '/autenticacao/getAutentication', 'as' => 'loginGoogle.getAutentication'], 'ControllerLoginGoogle@getAutentication');
+Route::get(['set' => '/autenticacao/loginValidation', 'as' => 'loginGoogle.loginValidation'], 'ControllerLoginGoogle@loginValidation');
+Route::post(['set' => '/autenticacao/loginValidation', 'as' => 'loginGoogle.loginValidation'], 'ControllerLoginGoogle@loginValidation');
