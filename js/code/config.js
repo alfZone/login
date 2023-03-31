503b3d4b0820536c18486401b0c3354fc0964c4b
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2023-03-31 16:52:44 +0100

    Update config.js

--- js/code/config.js
+++ js/code/config.js
@@ -1,15 +1,27 @@
 /**
  * @author alf
  * @copyright 2022
- * @ver 1.0
+ * @ver 1.1
  */
 
 
-class config {
-  constructor() {
+ class config {
+  
+  
+    constructor() {
+    }
+    
+    
+    color(pos){
+      let cores= ["bg-info", "bg-primary", "bg-success","bg-warning", "bg-danger", "bg-black","bg-indigo", "bg-fuchsia", "bg-navy","bg-info", "bg-primary", "bg-success","bg-warning", "bg-danger", "bg-black","bg-indigo", "bg-fuchsia", "bg-navy"];  
+      if (pos>cores.length){
+        pos=0;
+      }
+      return cores[pos];
+    }
+    
+    
+    get url() {
+      return  window.location.protocol + "//" + window.location.host + "/2do/";
+    }
   }
-
-  get url() {
-    return  window.location.protocol + "//" + window.location.host + "/";
-  }
-}
