dca021478b1690ab71e59aefaf1a80f1d5f1657f
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2026-01-13 16:01:25 +0000

    Fix route names for validaLogin authentication

--- src/routes/add - routes.php
+++ src/routes/add - routes.php
@@ -7,5 +7,5 @@ Route::get('/admin/in', function(){require _CAMINHO_TEMPLATE_ADMIN. "index.php";
 Route::get(['set' => '/autenticacao/logout', 'as' => 'loginGoogle.logout'], 'ControllerLoginGoogle@logout');
 Route::get(['set' => '/autenticacao/getAutentication', 'as' => 'LoginGoogle.getAutentication'], 'ControllerLoginGoogle@getAutentication');
 
-Route::get(['set' => '/autenticacao/validacaoLogin', 'as' => 'LoginGoogle.logout'], 'ControllerLoginGoogle@validaLogin');
-Route::post(['set' => '/autenticacao/validacaoLogin', 'as' => 'LoginGoogle.logout'], 'ControllerLoginGoogle@validaLogin');
+Route::get(['set' => '/autenticacao/validacaoLogin', 'as' => 'LoginGoogle.validaLogin'], 'ControllerLoginGoogle@validaLogin');
+Route::post(['set' => '/autenticacao/validacaoLogin', 'as' => 'LoginGoogle.validaLogin'], 'ControllerLoginGoogle@validaLogin');
