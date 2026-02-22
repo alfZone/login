06d71c7d2ac294b7cde55e9c6f0497c643b3246e
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2026-02-22 12:09:17 +0000

    Update authentication routes with new paths

--- src/routes/add - routes.php
+++ src/routes/add - routes.php
@@ -1,11 +1,14 @@
 ------ ADD to routes.php file do not replece
 
 //Authentication
-Route::get('/admin', function(){require _CAMINHO_TEMPLATE_ADMIN. "login.html";});
-Route::get('/admin/in', function(){require _CAMINHO_TEMPLATE_ADMIN. "index.php";});
+Route::get('/login', function(){require _PATH_FOR_ADMIN_TEMPLATE. "login.html";});  //path for login form
+Route::get('/admin', function(){require _PATH_FOR_ADMIN_TEMPLATE. "index.php";});   //path for backoffice land page
 
-Route::get(['set' => '/autenticacao/logout', 'as' => 'loginGoogle.logout'], 'ControllerLoginGoogle@logout');
-Route::get(['set' => '/autenticacao/getAutentication', 'as' => 'LoginGoogle.getAutentication'], 'ControllerLoginGoogle@getAutentication');
+Route::get(['set' => 'PATH_FOR_LOGOUT', 'as' => 'loginGoogle.logout'], 'ControllerLoginGoogle@logout');
+Route::get(['set' => 'PATH_TO_GET_AUTHENTICATION_INFORMATION', 'as' => 'LoginGoogle.getAutentication'], 'ControllerLoginGoogle@getAutentication');
 
-Route::get(['set' => '/autenticacao/validacaoLogin', 'as' => 'LoginGoogle.validaLogin'], 'ControllerLoginGoogle@validaLogin');
-Route::post(['set' => '/autenticacao/validacaoLogin', 'as' => 'LoginGoogle.validaLogin'], 'ControllerLoginGoogle@validaLogin');
+Route::get(['set' => 'PATH_TO_VALIDATE_LOGIN_GOOGLE', 'as' => 'LoginGoogle.validaLogin'], 'ControllerLoginGoogle@validaLogin');
+Route::post(['set' => 'PATH_TO_VALIDATE_LOGIN_GOOGLE', 'as' => 'LoginGoogle.validaLogin'], 'ControllerLoginGoogle@validaLogin');
+
+Route::get(['set' => 'PATH_TO_VALIDATE_LOGIN_SIMPLE', 'as' => 'LoginSimples.validaLogin'], 'ControllerLoginSimples@validaLogin');
+Route::post(['set' => 'PATH_TO_VALIDATE_LOGIN_SIMPLE', 'as' => 'LoginSimples.validaLogin'], 'ControllerLoginSimples@validaLogin');
