232f93719c812f2f28cc89d6b6609ddc2eb9350f
António Lira Fernandes <47450247+alfZone@users.noreply.github.com>
2026-03-25 12:26:03 +0000

    Add new routes for login and account recovery

--- src/routes/add - routes.php
+++ src/routes/add - routes.php
@@ -1,7 +1,10 @@
 ------ ADD to routes.php file do not replece
 
+Route::get('/login', function(){require _CAMINHO_TEMPLATE . "login.html";});
+Route::get('/criar-conta', function(){require _CAMINHO_TEMPLATE_ADMIN. "registar.html";});
+Route::get('/recuperar-password', function(){require _CAMINHO_TEMPLATE_ADMIN. "password-recover.html";});
+
 //Authentication
-Route::get('/login', function(){require _PATH_FOR_ADMIN_TEMPLATE. "login.html";});  //path for login form
 Route::get('/admin', function(){require _PATH_FOR_ADMIN_TEMPLATE. "index.php";});   //path for backoffice land page
 
 Route::get(['set' => 'PATH_FOR_LOGOUT', 'as' => 'loginGoogle.logout'], 'ControllerLoginGoogle@logout');
@@ -12,3 +15,9 @@ Route::post(['set' => 'PATH_TO_VALIDATE_LOGIN_GOOGLE', 'as' => 'LoginGoogle.vali
 
 Route::get(['set' => 'PATH_TO_VALIDATE_LOGIN_SIMPLE', 'as' => 'LoginSimples.validaLogin'], 'ControllerLoginSimples@validaLogin');
 Route::post(['set' => 'PATH_TO_VALIDATE_LOGIN_SIMPLE', 'as' => 'LoginSimples.validaLogin'], 'ControllerLoginSimples@validaLogin');
+
+
+Route::get(['set' => '/admin/recoverLoginSimple', 'as' => 'LoginSimples.recoverLogin'], 'ControllerLoginSimples@recoverLogin');
+Route::post(['set' => '/admin/recoverLoginSimple', 'as' => 'LoginSimples.recoverLogin'], 'ControllerLoginSimples@recoverLogin');
+Route::get(['set' => '/admin/recoverLoginSimple-s2', 'as' => 'LoginSimples.recoverLoginS2'], 'ControllerLoginSimples@recoverLoginS2');
+Route::post(['set' => '/admin/recoverLoginSimple-s2', 'as' => 'LoginSimples.recoverLoginS2'], 'ControllerLoginSimples@recoverLoginS2');
